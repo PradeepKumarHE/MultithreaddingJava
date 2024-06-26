b97bc936f2bd0b3fb7cbcbdb192f54ef7b6539c2
Pradeep <pradeepkumarhe1989@gmail.com>
2024-06-26 10:02:41 +0530

    Thread implementation

--- src/main/java/org/learnjava/service/ProductService.java
+++ src/main/java/org/learnjava/service/ProductService.java
@@ -1,8 +1,10 @@
 package org.learnjava.service;
 
+import lombok.Getter;
 import org.learnjava.domain.Product;
 import org.learnjava.domain.ProductInfo;
 import org.learnjava.domain.Review;
+
 import static org.learnjava.util.CommonUtil.stopWatch;
 import static org.learnjava.util.LoggerUtil.log;
 
@@ -15,18 +17,30 @@ public class ProductService {
         this.reviewService = reviewService;
     }
 
-    public Product retrieveProductDetails(String productId) {
+    public Product retrieveProductDetails(String productId) throws InterruptedException {
         stopWatch.start();
 
-        ProductInfo productInfo = productInfoService.retrieveProductInfo(productId); // blocking call
-        Review review = reviewService.retrieveReviews(productId); // blocking call
+        ProductInfoRunnable productInfoRunnable = new ProductInfoRunnable(productId);
+        Thread productInfoThread = new Thread(productInfoRunnable);
+
+        ReviewRunnable reviewInfoRunnable = new ReviewRunnable(productId);
+        Thread reviewThread = new Thread(reviewInfoRunnable);
+
+        productInfoThread.start();
+        reviewThread.start();
+
+        productInfoThread.join();
+        reviewThread.join();
+
+        ProductInfo productInfo=productInfoRunnable.getProductInfo();
+        Review review=reviewInfoRunnable.getReview();
 
         stopWatch.stop();
-        log("Total Time Taken : "+ stopWatch.getTime());
+        log("Total Time Taken : " + stopWatch.getTime());
         return new Product(productId, productInfo, review);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ProductInfoService productInfoService = new ProductInfoService();
         ReviewService reviewService = new ReviewService();
@@ -34,6 +48,39 @@ public class ProductService {
         String productId = "ABC123";
         Product product = productService.retrieveProductDetails(productId);
         log("Product is " + product);
+
+    }
+
+
+    private class ProductInfoRunnable implements Runnable {
+        @Getter
+        private ProductInfo productInfo;
+        private String productId;
+
+        public ProductInfoRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        @Override
+        public void run() {
+            productInfo = productInfoService.retrieveProductInfo(productId);
+        }
+
+    }
+
+    private class ReviewRunnable implements Runnable {
+        private String productId;
+        @Getter
+        private Review review;
+
+        public ReviewRunnable(String productId) {
+            this.productId = productId;
+        }
+
+        @Override
+        public void run() {
+            review = reviewService.retrieveReviews(productId);
+        }
 
     }
 }
